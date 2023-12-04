e50dbbfb858a025b544ac422bbe59852b2db3bc6
ronload <91997734+ronload@users.noreply.github.com>
2023-12-05 02:32:58 +0800

    v2023.12.05 completed.

--- static/script/index.js
+++ static/script/index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded', function () {
+    const form = document.querySelector('.form');
+    form.classList.add('fadeIn'); // 添加了 'fadeIn' 類別
     const calculateButton = document.querySelector('.calculate');
     const totalFundsInput = document.getElementById('total-funds');
     const acceptLossInput = document.getElementById('accept-loss');
     const strategyLossInput = document.getElementById('strategy-loss');
-    const form = document.querySelector('.form');
     const resultMessageContainer = document.querySelector('.result-message');
 
     calculateButton.addEventListener('click', function () {
@@ -14,7 +15,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // 檢查是否accept_loss超過5%
         if (acceptLoss > 5) {
-            const userConfirmation = confirm('警告：您的止损可能过高，有潜在的爆仓风险，仍要继续吗？');
+            const userConfirmation = confirm('警告：您的止損可能過高，有潛在的爆倉風險，仍要繼續嗎？');
 
             if (!userConfirmation) {
                 // 用戶選擇取消，清除輸入
@@ -34,16 +35,22 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // 創建新的結果元素
         const resultMessage = document.createElement('div');
-        // resultMessage.className = 'result-message';
-        // resultMessage.textContent = `您的可接受最大仓位为 ${maxPosition.toFixed(2)} USDT。`;
         resultMessage.className = 'result-message';
         resultMessage.innerHTML = `您的可接受最大仓位（含杠杆）为<br>${maxPosition.toFixed(2)} USDT。`;
-        
+
+        // 添加 show 類以啟動過渡效果
+        resultMessage.classList.add('show');
+
         // 清除之前的結果
-        resultMessageContainer.textContent = '';
-        
+        resultMessageContainer.innerHTML = '';
+
         // 顯示新的結果
         resultMessageContainer.appendChild(resultMessage);
-        
+
+        // 添加 show 類以啟動過渡效果
+        resultMessageContainer.classList.add('show');
+
+        // 設置 form 的高度為 auto
+        form.style.height = 'auto';
     });
 });
